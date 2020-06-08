62717cc7a652254144341fee1e8a182db2fc8f04
sanderzutt <43617759+sanderzutt@users.noreply.github.com>
2020-06-08 19:30:18 +0200

    updated css/js

--- js/js.js
+++ js/js.js
@@ -1,11 +1,19 @@
 //===========================================//
-//              Button color switch          //
+//                 tekst appear              //
 //===========================================//
 
-// var button = document.querySelector('.knoppie');
-// var text = document.querySelector('.test');
-//
-// //===========test============//
-// button.addEventListener("click", function() {
-//   text.classList.toggle("tijdelijk");
-// });
+//========= text 1 ========//
+var punt = document.querySelector('.dot-1');
+var text = document.querySelector('.text-1');
+
+punt.addEventListener("click", function() {
+  text.classList.toggle("invliegen");
+});
+
+//========= text 2 ========//
+var punttwee = document.querySelector('.dot-2');
+var texttwee = document.querySelector('.text-2');
+
+punttwee.addEventListener("click", function() {
+  texttwee.classList.toggle("invliegen-2");
+});
