fc54b6f72710474fce777eebb561d34f71c40723
sanderzutt <43617759+sanderzutt@users.noreply.github.com>
2020-06-10 22:57:36 +0200

    updated HTML/CSS/JS

--- js/js.js
+++ js/js.js
@@ -15,5 +15,29 @@ var punttwee = document.querySelector('.dot-2');
 var texttwee = document.querySelector('.text-2');
 
 punttwee.addEventListener("click", function() {
-  texttwee.classList.toggle("invliegen-2");
+  texttwee.classList.toggle("invliegen");
+});
+
+//========= text 3 ========//
+var puntdrie = document.querySelector('.dot-3');
+var textdrie = document.querySelector('.text-3');
+
+puntdrie.addEventListener("click", function() {
+  textdrie.classList.toggle("invliegen");
+});
+
+//========= text 4 ========//
+var puntvier = document.querySelector('.dot-4');
+var textvier = document.querySelector('.text-4');
+
+puntvier.addEventListener("click", function() {
+  textvier.classList.toggle("invliegen");
+});
+
+//========= text 5 ========//
+var puntvijf = document.querySelector('.dot-5');
+var textvijf = document.querySelector('.text-5');
+
+puntvijf.addEventListener("click", function() {
+  textvijf.classList.toggle("invliegen");
 });
